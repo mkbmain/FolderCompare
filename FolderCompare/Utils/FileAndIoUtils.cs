de8a26ef2374c48c86949811200c247f69a40ac8
MkbMain <michael.bourke@live.co.uk>
2021-12-26 19:45:44 +0000

    minor change

--- FolderCompare/Utils/FileAndIoUtils.cs
+++ FolderCompare/Utils/FileAndIoUtils.cs
@@ -25,15 +25,15 @@ namespace FolderCompare.Utils
             }
         }
 
+        private static readonly string[] Suffix = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
         public static string BytesToString(long byteCount)
         {
-            string[] suf = {"B", "KB", "MB", "GB", "TB", "PB", "EB"}; //Longs run out around EB
             if (byteCount == 0)
-                return "0" + suf[0];
+                return "0" + Suffix[0];
             var bytes = Math.Abs(byteCount);
             var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
             var num = Math.Round(bytes / Math.Pow(1024, place), 1);
-            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+            return (Math.Sign(byteCount) * num).ToString() + Suffix[place];
         }
 
         public static void DirectoryCopy(string sourceDirName, string destDirName)
